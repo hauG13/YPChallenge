7441b7e1bd85e5784a4c5deb7be82dd9d76917f4
Skrinikov <skrinikovd@gmail.com>
2017-01-21 13:07:55 -0500

    Add classes

---
+++ src/main/java/com/teamdawson/ypchallenge/SalesAPI.java
@@ -0,0 +1,11 @@
+package com.teamdawson.ypchallenge;
+
+/**
+ *
+ * @author Danieil Skrinikov
+ * @version 0.0.01
+ * @since
+ */
+public class SalesAPI {
+
+}
---
+++ src/main/java/com/teamdawson/ypchallenge/TwitterBotApp.java
@@ -0,0 +1,11 @@
+package com.teamdawson.ypchallenge;
+
+/**
+ *
+ * @author Danieil Skrinikov
+ * @version 0.0.01
+ * @since 2017-01-21
+ */
+public class TwitterBotApp {
+    
+}
