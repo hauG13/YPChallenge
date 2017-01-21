249f469f84f872488508a24d87b23a4a0081d4e4
Skrinikov <skrinikovd@gmail.com>
2017-01-21 16:35:41 -0500

    Add impl

--- src/main/java/com/teamdawson/ypchallenge/TwitterBot.java
+++ src/main/java/com/teamdawson/ypchallenge/TwitterBot.java
@@ -1,44 +1,58 @@
 package com.teamdawson.ypchallenge;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import twitter4j.Query;
+import twitter4j.QueryResult;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
 /**
- * Each object of this class represents a twitter bot.
- * This bot will run every 5 minutes and look for all tweets with "#askyp" then 
- * reply to them with the nearest place which matches their need.
- * 
+ * Each object of this class represents a twitter bot. This bot will run every 5
+ * minutes and look for all tweets with "#askyp" then reply to them with the
+ * nearest place which matches their need.
+ *
  * @author Danieil Skrinikov
  * @version 0.0.01
  * @since 2017-01-21
  */
 public class TwitterBot {
-    
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
+    private long latestID;
+
     /**
      * Default no parameter constructor for the Bot
      */
-    public TwitterBot(){
-        
+    public TwitterBot() {
+        latestID = -1;
     }
-    
+
     /**
      * Runs the bot.
      */
-    public void run(){
-        
+    public void run() {
+
         //Handle to twitter API
         Twitter twitter = TwitterFactory.getSingleton();
-        
+
         try {
-            
-            Status status = twitter.updateStatus("Hello Hau Gilles");
-            
+            Query query = new Query("\"#haugilles\"");
+
+            QueryResult result = twitter.search(query);
+
+            for (Status tweet : result.getTweets()) {
+                if (tweet.getId() != latestID) {
+                    twitter.updateStatus("@" + tweet.getUser().getScreenName() + " test.");
+                }
+                log.info(tweet.getId() + "");
+            }
+
+            //Status status = twitter.updateStatus("Hello Hau Gilles");
         } catch (TwitterException ex) {
-            Logger.getLogger(TwitterBot.class.getName()).log(Level.SEVERE, null, ex);
+            log.error("Connection failed.");
         }
     }
 }
